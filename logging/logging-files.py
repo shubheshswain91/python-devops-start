f15538b8f37e500a7e52baeb3dcab3424dfb1128
shubheshswain91 <shubheshswain91@gmail.com>
2025-10-16 11:14:44 +0200

    timed based rotating

--- logging/logging-files.py
+++ logging/logging-files.py
@@ -3,6 +3,12 @@ import os
 import logging.handlers
 import time
 
+
+def cleanup_log_files(base_name: str):
+    for file_name in os.listdir("."):
+        if file_name.startswith(base_name):
+            os.remove(file_name) 
+
 # Basic logging with FileHandler
 print("basic logging with FileHandler")
 print("-------------------------------\n")
@@ -24,9 +30,7 @@ print("---------------------------------\n")
 
 rotating_logs_filename = "rotatingfile.log"
 
-for file_name in os.listdir("."):
-    if file_name .startswith(rotating_logs_filename):
-        os.remove(file_name)
+cleanup_log_files(rotating_logs_filename)
 
 rotatting_logger = logging.getLogger("file.rotating")
 rotatting_logger.setLevel(logging.DEBUG)
@@ -45,3 +49,28 @@ for i in range(30):
     time.sleep(0.05)
 
 # Time-based log rotation with TimedRotatingFileHandler
+
+print("\ntime-based log rotation with TimedRotatingFileHandler")
+print("---------------------------------\n")
+
+timed_rotating_logs_filename = "timedrotatingfile.log"
+
+cleanup_log_files(timed_rotating_logs_filename)
+
+timed_rotating_logger = logging.getLogger("file.timed")
+timed_rotating_logger.setLevel(logging.DEBUG)
+
+timed_rotating_fh = logging.handlers.TimedRotatingFileHandler(
+    timed_rotating_logs_filename, 
+    when="S", 
+    backupCount=2,
+    encoding="utf-8"
+    )
+
+timed_rotating_fh.setFormatter(logging.Formatter("%(levelname)-8s %(message)s"))
+
+timed_rotating_logger.addHandler(timed_rotating_fh)
+
+for i in range(30):
+    timed_rotating_logger.info(f"Entry {i}: {'Z' * 50}")
+    time.sleep(0.5)
