fa0909fc88be91ec0922e5c2b565222598f9b0af
shubheshswain91 <shubheshswain91@gmail.com>
2025-10-22 11:08:18 +0200

    Introduction to type hints

--- typing/typing_basics.py
+++ typing/typing_basics.py
@@ -1,6 +1,21 @@
 # Section: Basic Type Hint Syntax - Variable Annotations
 
+config_path: str = "/path/to/config.ini"
+retry_count: int = 3
+is_enalbed: bool = bool(1)
+servers: list[str] = ["server1", "server2", "server3"]
+settings: dict[str, int | str] = {
+    "port": 8080,
+    "username": "admin"
+} 
+
 # Section: Basic Type Hint Syntax - Function Argument and Return Type Annotations
+def get_server_status(hostaname: str, port: int) -> str:
+    print(f"Checking {hostaname}")
+    if port == 80:
+        return "Server is up"
+    else:
+        return "Server is down"
 
 # Section: Python Remains Dynamically Typed
 
