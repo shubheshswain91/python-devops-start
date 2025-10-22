4b6b0b3ef3c395aa76ba9add1720b25d4998c133
shubheshswain91 <shubheshswain91@gmail.com>
2025-10-22 15:41:25 +0200

    Typing lists

--- typing/common_types.py
+++ typing/common_types.py
@@ -1,5 +1,14 @@
 # Section: Typing Lists
 
+hostnames: list[str]= ['web01.example1.com', 'db01.example2.com', 'example3.com']
+open_ports: list[int] = [80, 443, 8080, 22]
+
+def process_hostnames(hostnames: list[str]) -> None:
+    for hostname in hostnames:
+        print(f"Processing hostname: {hostname.upper()}")
+
+process_hostnames(hostnames)
+# process_hostname(open_ports)  # This will raise a TypeError
 # Section: Typing Dictionaries
 
 # Section: Typing Tuples
