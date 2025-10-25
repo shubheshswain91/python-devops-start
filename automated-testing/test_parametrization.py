785d4612e547f3fb582402f6869d34c712986979
shubheshswain91 <shubheshswain91@gmail.com>
2025-10-26 00:47:15 +0200

    intro to parametrization

--- automated-testing/test_parametrization.py
+++ automated-testing/test_parametrization.py
@@ -25,6 +25,46 @@ def check_url_status(url: str) -> tuple[int | str, str]:
 
 # Section: The Problem: Duplicated Test Logic
 
+""" 
+a -> True
+5 -> True
+- -> True
+A -> False
+_ -> False
+ """
+
+def test_is_valid_lowercase_char() -> None:
+    assert is_valid_hostname_char("a") is True
+
+def test_is_valid_5() -> None:    
+    assert is_valid_hostname_char("5") is True
+
+def  test_is_valid_hyphen() -> None:    
+    assert is_valid_hostname_char("-") is True
+
+def test_is_valid_uppercase_char() -> None:    
+    assert is_valid_hostname_char("A") is False    
+
+def test_is_valid_underscore() -> None:    
+    assert is_valid_hostname_char("_") is False    
+
 # Section: Solution: @pytest.mark.parametrize
+
+@pytest.mark.parametrize(
+    "input_char, expected_result",
+    [
+        ("a", True),
+        ("5", True), 
+        ("-", True), 
+        ("A", False), 
+        ("_", False)  # more test cases...  # (Note: You can add as many test cases as you want)
+    ]
+)
+
+def test_is_valid_hostname_char(
+    input_char: str,
+    expected_result: bool,
+):
+    assert is_valid_hostname_char(input_char) is expected_result
 
 # Section: Customizing Test IDs with pytest.param construct
