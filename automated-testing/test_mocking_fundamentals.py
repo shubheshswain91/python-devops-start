95c7b907ecba605a09e1d39ccd01f57c1477c126
shubheshswain91 <shubheshswain91@gmail.com>
2025-10-26 16:21:32 +0100

    patch decorator and mocker fixture

--- automated-testing/test_mocking_fundamentals.py
+++ automated-testing/test_mocking_fundamentals.py
@@ -1,5 +1,6 @@
-from unittest.mock import patch, Mock 
+from unittest.mock import patch, Mock, MagicMock
 from dummy_functions import check_file_exists 
+from pytest_mock import MockerFixture
 
 # Section: Using unittest.mock.patch
 
@@ -28,6 +29,24 @@ def test_check_file_exists_context_manager() -> None:
         result = check_file_exists(filepath=filepath)
         mock_exists.assert_called_once_with(filepath)
         assert result is True
+
+@patch("dummy_functions.os.path.exists")
+def test_check_file_exists_decorator(mock_exists: Mock) -> None:
+    filepath = "/path/to/some/file.txt"
+    mock_exists.return_value = True
+    result = check_file_exists(filepath=filepath)
+    mock_exists.assert_called_once_with(filepath)
+    assert result is True        
     
+
+def test_check_file_pytest_mocker(mocker: MockerFixture) -> None:
+    filepath = "/path/to/some/file.txt"
+
+    mock_exists = mocker.patch("dummy_functions.os.path.exists")
+
+    mock_exists.return_value = True
+    result = check_file_exists(filepath=filepath)
+    mock_exists.assert_called_once_with(filepath)
+    assert result is True    
 
 # Section: MagicMock and Configuring Mock Objects
