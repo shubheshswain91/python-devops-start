b5489ed6523519d90a792eab8757c8c16075dc50
shubheshswain91 <shubheshswain91@gmail.com>
2025-10-26 20:07:10 +0100

    Python modules

---
+++ multi-file-projects/main.py
@@ -0,0 +1,12 @@
+print("Main script starting")
+
+from file_ops import check_file_extension as check_file
+import sys
+
+print(sys.path)
+
+filenames = ["config.yaml", "script.sh", "readit.go"]
+
+for filename in filenames:
+    print(f"Checking {filename}")
+    print(f"Results: {check_file(filename)}")
